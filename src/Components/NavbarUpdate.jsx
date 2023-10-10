a2ed49def53810fe3316142d064e1f9d11f7fb69
MarwanAshrafKhalil <marwan.a.k.1997@gmailcom>
2023-10-10 21:00:37 +0300

    navbar- En/Ar fix && tracking id

--- src/Components/NavbarUpdate.jsx
+++ src/Components/NavbarUpdate.jsx
@@ -1,19 +1,8 @@
-import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useNavigate } from "react-router-dom";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment, useState } from "react";
 import { useTranslation } from "react-i18next";
-
-const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "Pricing", href: "#", current: false },
-  { name: "Contact Us", href: "#", current: false },
-];
-const navigation2 = [
-  { name: "Track Shipment", href: "#", current: false },
-  { name: "Signin", href: "#", current: false },
-  { name: "AR", href: "#", current: false },
-];
+import { useNavigate } from "react-router-dom";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -24,6 +13,17 @@ export default function NavbarUpdate() {
   const [open, setOpen] = useState(false);
   const [trackID, setTrackID] = useState("");
   const [t, i18n] = useTranslation();
+
+  const navigation = [
+    { name: t("Home"), href: "/" },
+    { name: t("Pricing"), href: "#" },
+    { name: t("Contact Us"), href: "#" },
+  ];
+  const navigation2 = [
+    { name: t("Track Shipping"), href: "#" },
+    { name: t("Signin"), href: "#" },
+    // { name: "AR", href: "#" },
+  ];
 
   function updateTrackID(ID) {
     setTrackID(ID);
@@ -42,7 +42,7 @@ export default function NavbarUpdate() {
     <Disclosure as="nav" className="bg-white pt-10 pe-70 pb-16 ps-70">
       {({ open }) => (
         <>
-          <div className="mx-auto  px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-[1200px]  px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16  items-center justify-between">
               <div className="absolute  left-0 pl-5 flex  sm:hidden">
                 {/* Mobile menu button*/}
@@ -56,151 +56,162 @@ export default function NavbarUpdate() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex w-full gap-10 items-center justify-center  sm:flex sm:justify-between">
-                <div id="logo" className="flex justify-center items-center ">
+              <div className="flex  w-full gap-10 items-center justify-center  sm:flex sm:justify-between">
+                <div id="logo" className="flex flex-shrink-0">
                   <img
                     className="h-8 w-auto"
                     src="https://lever-client-logos.s3.us-west-2.amazonaws.com/4a536633-277c-4c83-afa7-586bfcce7dd0-1636574104926.png"
                     alt="Your Company"
                   />
                 </div>
-                <div id="nav-content" className="hidden flex pl-10 flex-grow items-center justify-between  me-10 gap-10 sm:flex ">
-                  <div id="first nav" className="hidden pl-10 md:flex space-x-4 ">
+                <div
+                  id="nav-content"
+                  className="hidden flex flex-grow max-w-[800px] justify-center md:justify-between  me-16  sm:flex "
+                >
+                  <div
+                    id="first nav"
+                    className="hidden  gap-4 pl-10 md:flex space-x-4 "
+                  >
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : " hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        className="btn-primary"
                       >
                         {item.name}
                       </a>
                     ))}
                   </div>
-                  <div id="second nav" className=" flex space-x-4">
-                  {navigation2.map((item) => (
-                    <ul
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : " text-black hover:bg-gray-700 hover:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      <li>
-                        {item.name != "Track Shipment" ? (
-                          item.name
-                        ) : (
-                          <Menu
-                            as="div"
-                            className="relative ml-3"
-                            // onMouseLeave={() => setOpen(false)}
-                            // onMouseEnter={() => setOpen(true)}
-                          >
-                            <div
-                            // className={open ? "block" : "hidden"}
+                  <div id="second nav" className=" flex  gap-4 space-x-4">
+                    {navigation2.map((item) => (
+                      <ul
+                        key={item.name}
+                        href={item.href}
+                        className="btn-primary"
+                      >
+                        <li>
+                          {item.name != "Track Shipment" ? (
+                            item.name
+                          ) : (
+                            <Menu
+                              as="div"
+                              className="relative ml-3"
+                              // onMouseLeave={() => setOpen(false)}
+                              // onMouseEnter={() => setOpen(true)}
                             >
-                              <Menu.Button className="relative flex ">
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">Open user menu</span>
-                                <li>{item.name}</li>
-                              </Menu.Button>
-                            </div>
-                            <Transition
-                              as={Fragment}
-                              enter="transition ease-out duration-100"
-                              enterFrom="transform opacity-0 scale-95"
-                              enterTo="transform opacity-100 scale-100"
-                              leave="transition ease-in duration-75"
-                              leaveFrom="transform opacity-100 scale-100"
-                              leaveTo="transform opacity-0 scale-95"
-                            >
-                              <Menu.Items className="absolute   left-0  z-10 mt-2 py-2 w-50 h-44 rounded-lg shadow-xl bg-white border border-1">
-                                <div className=" flex w-full flex-col m-4">
-                                  <label className=" w-full left-0  text-grayish font-medium text-lg">
-                                    {t("Track Shipping")}
-                                  </label>
-                                  <div className="flex content-center mt-5 ">
-                                    <input
-                                      className="border px-2 w-60 border-grayish  rounded-s-lg py-1 "
-                                      type="text"
-                                      placeholder="Enter the Shipment ID"
-                                      value={trackID}
-                                      onChange={(ev) =>
-                                        updateTrackID(ev.target.value)
-                                      }
-                                    />
-                                    <button
-                                      onClick={() => searchID()}
-                                      className=" bg-bosta w-14 h-14 items-center text-white rounded-e-xl"
-                                    >
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1}
-                                        stroke="currentColor"
-                                        className="ms-2 w-9 h-9"
+                              <div
+                              // className={open ? "block" : "hidden"}
+                              >
+                                <Menu.Button className="relative flex ">
+                                  <span className="absolute -inset-1.5" />
+                                  <span className="sr-only">
+                                    Open user menu
+                                  </span>
+                                  <li>{item.name}</li>
+                                </Menu.Button>
+                              </div>
+                              <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                              >
+                                <Menu.Items className="absolute   left-0  z-10 mt-2 py-2 w-50 h-44 rounded-lg shadow-xl bg-white border border-1">
+                                  <div className=" flex w-full flex-col m-4">
+                                    <label className=" w-full left-0  text-grayish font-medium text-lg">
+                                      {t("Track Shipping")}
+                                    </label>
+                                    <div className="flex content-center mt-5 ">
+                                      <input
+                                        className="border px-2 w-60 border-grayish  rounded-s-lg py-1 "
+                                        type="text"
+                                        placeholder="Enter the Shipment ID"
+                                        value={trackID}
+                                        onChange={(ev) =>
+                                          updateTrackID(ev.target.value)
+                                        }
+                                      />
+                                      <button
+                                        onClick={() => searchID()}
+                                        className=" bg-bosta w-14 h-14 items-center text-white rounded-e-xl"
                                       >
-                                        <path
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                                        />
-                                      </svg>
-                                    </button>
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          fill="none"
+                                          viewBox="0 0 24 24"
+                                          strokeWidth={1}
+                                          stroke="currentColor"
+                                          className="ms-2 w-9 h-9"
+                                        >
+                                          <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                                          />
+                                        </svg>
+                                      </button>
+                                    </div>
                                   </div>
-                                </div>
-                              </Menu.Items>
-                            </Transition>
-                          </Menu>
+                                </Menu.Items>
+                              </Transition>
+                            </Menu>
+                          )}
+                        </li>
+                      </ul>
+                      // <a
+                      //   key={item.name}
+                      //   href={item.href}
+                      //   className={classNames(
+                      //     item.current
+                      //       ? "bg-gray-900 text-white"
+                      //       : " text-black hover:bg-gray-700 hover:text-white",
+                      //     "rounded-md px-3 py-2 text-sm font-medium"
+                      //   )}
+                      //   aria-current={item.current ? "page" : undefined}
+                      // >
+                      //   {item.name}
+                      // </a>
+                    ))}
+
+                    <ul className="btn-primary">
+                      <li>
+                        {i18n.language == "en" && (
+                          <button
+                            onClick={() => {
+                              i18n.changeLanguage("ar");
+                            }}
+                          >
+                            AR
+                          </button>
+                        )}
+                        {i18n.language == "ar" && (
+                          <button
+                            onClick={() => {
+                              i18n.changeLanguage("en");
+                            }}
+                          >
+                            EN
+                          </button>
                         )}
                       </li>
                     </ul>
-                    // <a
-                    //   key={item.name}
-                    //   href={item.href}
-                    //   className={classNames(
-                    //     item.current
-                    //       ? "bg-gray-900 text-white"
-                    //       : " text-black hover:bg-gray-700 hover:text-white",
-                    //     "rounded-md px-3 py-2 text-sm font-medium"
-                    //   )}
-                    //   aria-current={item.current ? "page" : undefined}
-                    // >
-                    //   {item.name}
-                    // </a>
-                  ))}
-                </div>
+                  </div>
                 </div>
               </div>
-              
-              
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+          <Disclosure.Panel className="sm:hidden flex">
+            <div className="flex flex-col  items-left space-y-1 px-4 pb-3 pt-2">
               {navigation2.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as="a"
                   href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
+                  className="btn-primary"
                 >
                   {item.name}
                 </Disclosure.Button>
