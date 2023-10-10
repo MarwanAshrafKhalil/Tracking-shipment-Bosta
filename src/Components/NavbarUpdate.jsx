ae08939d6f99133c153c18a75ebcdff4e4620146
MarwanAshrafKhalil <marwan.a.k.1997@gmailcom>
2023-10-10 18:55:19 +0300

    navbar flex

--- src/Components/NavbarUpdate.jsx
+++ src/Components/NavbarUpdate.jsx
@@ -39,12 +39,12 @@ export default function NavbarUpdate() {
     navigate(`/Brief/${trackID}`);
   }
   return (
-    <Disclosure as="nav" className="bg-white">
+    <Disclosure as="nav" className="bg-white pt-10 pe-70 pb-16 ps-70">
       {({ open }) => (
         <>
           <div className="mx-auto  px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="relative flex h-16  items-center justify-between">
+              <div className="absolute  left-0 pl-5 flex  sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -56,17 +56,16 @@ export default function NavbarUpdate() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 gap-10 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+              <div className="flex w-full gap-10 items-center justify-center  sm:flex sm:justify-between">
+                <div id="logo" className="flex justify-center items-center ">
                   <img
                     className="h-8 w-auto"
                     src="https://lever-client-logos.s3.us-west-2.amazonaws.com/4a536633-277c-4c83-afa7-586bfcce7dd0-1636574104926.png"
                     alt="Your Company"
                   />
                 </div>
-
-                <div className="hidden flex  me-10  gap-12 sm:ml-6 sm:flex ">
-                  <div className="hidden  md:flex space-x-4 ">
+                <div id="nav-content" className="hidden flex pl-10 flex-grow items-center justify-between  me-10 gap-10 sm:flex ">
+                  <div id="first nav" className="hidden pl-10 md:flex space-x-4 ">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -83,7 +82,7 @@ export default function NavbarUpdate() {
                       </a>
                     ))}
                   </div>
-                  <div className=" flex space-x-4">
+                  <div id="second nav" className=" flex space-x-4">
                   {navigation2.map((item) => (
                     <ul
                       key={item.name}
@@ -124,8 +123,8 @@ export default function NavbarUpdate() {
                               leaveFrom="transform opacity-100 scale-100"
                               leaveTo="transform opacity-0 scale-95"
                             >
-                              <Menu.Items className="absolute  left-0 md:right-0 z-10 mt-2 py-2 w-50 h-44 rounded-lg shadow-xl bg-white border border-1">
-                                <div className=" flex  flex-col m-4">
+                              <Menu.Items className="absolute   left-0  z-10 mt-2 py-2 w-50 h-44 rounded-lg shadow-xl bg-white border border-1">
+                                <div className=" flex w-full flex-col m-4">
                                   <label className=" w-full left-0  text-grayish font-medium text-lg">
                                     {t("Track Shipping")}
                                   </label>
@@ -184,81 +183,7 @@ export default function NavbarUpdate() {
                 </div>
               </div>
               
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-                {/* Profile dropdown */}
-                {/* <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu> */}
-              </div>
+              
             </div>
           </div>
 
